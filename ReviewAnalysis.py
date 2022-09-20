268e272389b9e9d8bb312ca4cf1783d65d4df101
Balakumaran_E <balakumaran242002@gmail.com>
2022-09-20 15:10:17 +0530

    Update ReviewAnalysis.py

--- ReviewAnalysis.py
+++ ReviewAnalysis.py
@@ -1,17 +1,5 @@
 import pickle
 import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
-import nltk
-import re
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score , classification_report , confusion_matrix
-import pickle
 save_cv = pickle.load(open('count-Vectorizer.sav','rb'))
 model = pickle.load(open('Review_Classification.sav', 'rb'))
 
